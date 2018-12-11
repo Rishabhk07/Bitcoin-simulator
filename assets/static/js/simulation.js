50be517203d5b87f3e765a80c2bedfd3f7c09e1a
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-12-11 02:24:06 -0500

    charts working on my command
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- assets/static/js/simulation.js
+++ assets/static/js/simulation.js
@@ -1,84 +1,18 @@
 var blockchain = "";
 $(function (){
-   console.log("Simulation started");
-
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
-    });
+    var wallet_chart;
+    var miner_chart;
+    console.log("Simulation started");
 
 
-    var ctx2 = document.getElementById("myChart2").getContext('2d');
-    var myChart2 = new Chart(ctx2, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
-    });
-
+   init_wallet_chart();
+   init_miner_chart();
+   
+   $("#start-simulation").click(function () {
+      data = [50,34,50,29,90,70];
+       update_miner_data(data);
+   });
+   
 
    $.get("/simulate", function (data) {
        console.log(data);
@@ -110,4 +44,91 @@ $(function (){
 
       })
     }
+    
+    function update_miner_data(new_data) {
+        miner_chart.data.datasets[0].data = new_data;
+        miner_chart.update();
+    }
+
+    function init_wallet_chart(){
+        var ctx = document.getElementById("wallet_chart").getContext('2d');
+        wallet_chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Wallet1", "Wallet2", "Wallet3", "Wallet4", "Wallet5", "Wallet6"],
+                datasets: [{
+                    label: '# Wallet with bitcoins',
+                    data: [11, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+   
+    
+    function init_miner_chart() {
+        var ctx2 = document.getElementById("miner_chart").getContext('2d');
+        miner_chart = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: ["Miner1", "Miner2", "Miner3", "Miner4", "Miner5", "Miner6"],
+                datasets: [{
+                    label: '# Miner with bitcoins',
+                    data: [0, 1, 2, 3, 4, 5],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+
 });
